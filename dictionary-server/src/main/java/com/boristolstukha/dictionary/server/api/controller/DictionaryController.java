596b3a7f5cb5669f073a3f4cca84fe0fc12e5d51
Boris Tolstukha <boris.tolstukha@gmail.com>
2017-08-26 22:43:58 +0600

    adding and deletion

--- dictionary-server/src/main/java/com/boristolstukha/dictionary/server/api/controller/DictionaryController.java
+++ dictionary-server/src/main/java/com/boristolstukha/dictionary/server/api/controller/DictionaryController.java
@@ -5,6 +5,7 @@ import com.boristolstukha.dictionary.logic.Dictionary;
 import com.boristolstukha.dictionary.server.api.exceptions.NotFoundHttpException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -18,7 +19,7 @@ public class DictionaryController {
     }
 
     @RequestMapping(value = "/{word}", method = RequestMethod.GET)
-    public DefinitionSetDTO getDefinitions(@PathVariable("word") String word) throws NotFoundHttpException{
+    public DefinitionSetDTO getDefinitions(@PathVariable("word") String word) throws NotFoundHttpException {
         Set<String> definitions = dictionary.get(word);
         if(definitions == null){
             throw new NotFoundHttpException("Слово отсутвует в словаре");
@@ -27,6 +28,20 @@ public class DictionaryController {
         definitionSetDTO.word = word;
         definitionSetDTO.definitions = definitions;
         return definitionSetDTO;
+    }
+
+    @RequestMapping(value = "/{word}", method = { RequestMethod.POST, RequestMethod.PUT })
+    public void postDefinitions(@PathVariable("word") String word,
+                                @RequestBody List<String> definitions){
+        dictionary.add(word, definitions);
+    }
+
+    @RequestMapping(value = "/{word}", method = { RequestMethod.DELETE })
+    public void deleteDefinitions(@PathVariable("word") String word,
+                                @RequestBody List<String> definitions) throws NotFoundHttpException {
+        if(!dictionary.delete(word, definitions)){
+            throw new NotFoundHttpException("Cлово либо значение отсутвует в словаре");
+        }
     }
 
 }
