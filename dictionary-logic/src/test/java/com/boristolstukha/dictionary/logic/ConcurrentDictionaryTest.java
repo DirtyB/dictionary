1d65e99806e9e20aaa0276d65504367d4b41223a
Boris Tolstukha <boris.tolstukha@gmail.com>
2017-08-26 20:16:56 +0600

    second unit test

--- dictionary-logic/src/test/java/com/boristolstukha/dictionary/logic/ConcurrentDictionaryTest.java
+++ dictionary-logic/src/test/java/com/boristolstukha/dictionary/logic/ConcurrentDictionaryTest.java
@@ -35,4 +35,46 @@ public class ConcurrentDictionaryTest {
 
     }
 
+    @Test
+    public void deleteTest(){
+
+        Dictionary dictionary = new ConcurrentDictionary();
+
+        List<String> meanings = new ArrayList<String>();
+        meanings.add("привет");
+        meanings.add("алло");
+        meanings.add("здравствуйте");
+
+        assertTrue(dictionary.add("goodbye", "до свидания"));
+
+        assertTrue(dictionary.add("hello", "до свидания"));
+        assertTrue(dictionary.add("hello", meanings));
+
+        assertTrue(dictionary.delete("hello","до свидания"));
+
+        Set<String> actual = dictionary.get("hello");
+
+        assertEquals(3,actual.size());
+        for(String value: meanings){
+            assertTrue(actual.contains(value));
+        }
+
+        assertFalse(dictionary.delete("hello","до свидания"));
+
+        assertTrue(dictionary.add("hello", "здрасте"));
+
+        assertTrue(dictionary.delete("hello", meanings));
+
+        actual = dictionary.get("hello");
+        assertEquals(1,actual.size());
+        assertTrue(actual.contains("здрасте"));
+
+        assertFalse(dictionary.delete("hello", meanings));
+
+        actual = dictionary.get("goodbye");
+        assertEquals(1,actual.size());
+        assertTrue(actual.contains("до свидания"));
+
+    }
+
 }
