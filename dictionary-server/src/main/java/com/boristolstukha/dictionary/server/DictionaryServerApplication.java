60a2887b071404d5c5a7805f3e03d6018a736103
Boris Tolstukha <boris.tolstukha@gmail.com>
2017-08-26 17:04:27 +0600

    initalized multi mudule app & server module

---
+++ dictionary-server/src/main/java/com/boristolstukha/dictionary/server/DictionaryServerApplication.java
@@ -0,0 +1,12 @@
+package com.boristolstukha.dictionary.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryServerApplication.class, args);
+	}
+}
